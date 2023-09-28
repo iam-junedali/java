1f0768cc3140509edbc8b53579bdcc75cb417aa0
junedali1995 <junedali.sk@gmail.com>
2023-09-28 20:59:13 +0530

    new project

---
+++ java/src/main/java/org/example/arithematicoperators.java
@@ -0,0 +1,18 @@
+package org.example;
+
+public class arithematicoperators {
+
+    public static void main(String[] args) {
+
+        //Logical Operators
+        int a = 20;
+        int b = 20;
+        int c = a + b;
+
+        System.out.println(c);
+
+
+
+    }
+
+}
